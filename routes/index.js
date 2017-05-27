7ed48a77a830ad0692107cf770b39baf47defe58
Kevin Wong <kkwokwai22@gmail.com>
2017-05-27 15:41:12 -0700

    implement nodemailer and created a route to send email implement fs to make a path to read index.html to send through as email.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+var fs = require('fs');
 
+
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'prompttesting@gmail.com',
+        pass: 'testing111'
+    }
+});
+
+var htmlTemplate = fs.readFileSync('./views/index.html', 'utf8');
+
+// setup email data with unicode symbols
+let mailOptions = {
+    from: '"Kevin Wong 👻" <foo@blurdybloop.com>', // sender address
+    to: 'kkwok_wai@hotmail.com', // list of receivers
+    subject: 'welcome to nodemailer', // Subject line
+    text: 'Testing with Kevin!', // plain text body
+    html: htmlTemplate// html body
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/sendEmail', function(req, res, next) {
+    res.send(transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+    }));
+})
+
 
 module.exports = router;
 
