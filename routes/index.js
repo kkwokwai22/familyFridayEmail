d099f64784177905ae69a7cd0fbfe1be5c1c21e6
Kevin Wong <kkwokwai22@gmail.com>
2017-05-28 16:09:47 -0700

    add a callback on to ejs.rendefile now nodemailer can send ejs file with dynamtic content still need testing to finalize

--- routes/index.js
+++ routes/index.js
@@ -54,26 +54,29 @@ router.get('/sendEmail', function(req, res, next) {
     });
 
     // sending the html template as email 
-    var EjsTemplate = fs.readFileSync('./views/index.ejs', 'utf8');
-
-
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Kevin Wong 👻" <prompttesting@gmail.com>', // sender address
-        to: 'kkwok_wai@hotmail.com', // list of receivers
-        subject: 'welcome to nodemailer', // Subject line
-        text: 'Testing with Kevin!', // plain text body
-        html: EjsTemplate// html body
-    };
-
+    ejs.renderFile('./views/index.ejs', {user:"great user", title:"homepage"}, 
+        function( err, data) {
+            if(err) {
+                console.log(err);
+            } else {
+                // setup email data with unicode symbols
+                let mailOptions = {
+                    from: '"Kevin Wong 👻" <prompttesting@gmail.com>', // sender address
+                    to: 'kkwok_wai@hotmail.com', // list of receivers
+                    subject: 'welcome to nodemailer', // Subject line
+                    text: 'Testing with Kevin!', // plain text body
+                    html: data// html body
+                };
+                transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return console.log(error);
+                }
+                console.log('Message %s sent: %s', info.messageId, info.response);
+                });
+            }
+        })
     res.render('index', {user: "Great User",title:"homepage"})
 
-    transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        return console.log(error);
-    }
-    console.log('Message %s sent: %s', info.messageId, info.response);
-    });
 })
 
 
